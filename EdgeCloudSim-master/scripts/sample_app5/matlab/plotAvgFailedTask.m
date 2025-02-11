fd7229e28a18a865cf3dc2af1bcb55430e31658e
Stelios Charilaou <stelioscha@live.com>
2025-02-11 18:52:33 +0000

    Remove previously tracked files now ignored by .gitignore

--- EdgeCloudSim-master/scripts/sample_app5/matlab/plotAvgFailedTask.m
+++
@@ -1,8 +0,0 @@
-function [] = plotAvgFailedTask()
-
-     plotGenericLine(1, 2, 'Average Failed Tasks (%)', 'ALL_APPS', 'NorthWest', 1);
-     plotGenericLine(1, 2, 'Failed Tasks for Danger Assessment App (%)', 'DANGER_ASSESSMENT', 'NorthWest', 1);
-     plotGenericLine(1, 2, 'Failed Tasks for Navigation App (%)', 'TRAFFIC_MANAGEMENT', 'NorthWest', 1);
-     plotGenericLine(1, 2, 'Failed Tasks for Infotainment App (%)', 'INFOTAINMENT', 'NorthWest', 1);
-     
-end
--- EdgeCloudSim-master/scripts/sample_app5/matlab/plotAvgVmUtilization.m
+++
@@ -1,7 +0,0 @@
-function [] = plotAvgVmUtilization()
-
-    plotGenericLine(2, 8, 'Average VM Utilization of RSU (%)', 'ALL_APPS', 'NorthWest', 0);
-
-    plotGenericLine(3, 8, 'Average VM Utilization of Cloud (%)', 'ALL_APPS', 'NorthWest', 0);
-    
-end
